aba68e8b67a834bd0f79be7555cf10d28a37122a
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-03-24 13:03:29 -0700

    Implement delete of a question

--- app/routes/question.js
+++ app/routes/question.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    delete(question) {
+      if (confirm('Delete question: "' + question.get('qu_title') + '"?')) {
+        question.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
   }
 });
