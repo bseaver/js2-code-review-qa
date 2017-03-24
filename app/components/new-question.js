4f209b3919200a65242d7e13db9678ce5a6aed56
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-03-24 11:46:27 -0700

    Clear new question form after save

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -15,6 +15,9 @@ export default Ember.Component.extend({
       };
       this.set('addNewQuestion', false);
       this.sendAction('saveQuestion', params);
+      this.set('qu_author', '');
+      this.set('qu_title', '');
+      this.set('qu_notes', '');
     }
   }
 });
